3dfe4237767818d9aeb26912723ec0754c4e2e82
soph-aslt32 <aslt3turnersoph@gmail.com>
2026-02-05 17:40:08 +0900

    [update]Update __dev/VSCode/settings.json

---
+++ __dev/VSCode/_test.py
@@ -0,0 +1,13 @@
+class TestClass:
+    def __init__(self) -> None:
+        pass
+
+def test_function() -> TestClass:
+    return TestClass()
+
+if __name__ == "__main__":
+    print("Hello, World!")
+
+    test_instance = test_function()
+
+    bracket_test = ((((((42))))))
